4d767b1342eb316256bd2f58f14996268fdfc0a3
Simon Günther <simon.guenther@sucurema.com>
2022-08-22 12:50:03 +0200

    ...

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -4,24 +4,6 @@ if not status_ok then
 end
 
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-}
-dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-}
-
 local function footer()
 	-- NOTE: requires the fortune-mod package to work
 	local handle = io.popen("fortune")
@@ -31,12 +13,48 @@ local function footer()
 	-- return "chrisatmachine.com"
 end
 
-dashboard.section.footer.val = footer()
+dashboard.section = {
+	{
+		{
+			type = "header",
+			val = {
 
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
-
-dashboard.opts.opts.noautocmd = true
+				[[                               __                ]],
+				[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+				[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+				[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+				[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+				[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+			},
+			opts = {
+				hl = "Include",
+				padding = 3,
+			},
+		},
+		{
+			type = "buttons",
+			val = {
+				dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+				dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+				dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+				dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+				dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+				dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+			},
+			opts = {
+				hl = "Keyword",
+			},
+		},
+		{
+			type = "footer",
+			val = footer(),
+			opts = {
+				hl = "Type",
+			},
+		},
+	},
+}
+-- dashboard.opts.opts.noautocmd = true
 -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
